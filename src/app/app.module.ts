f8eea0c432ce6545beb261d90d3ab8edc154d566
Adil Farooq <adilfarooq.it@gmail.com>
2021-12-12 08:59:41 +0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TaskComponent } from './components/task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    TaskComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+
+  public tasks = [
+    {id: 1, title: 'Association  with database'},
+    {id: 2, title: 'Databse linking'},
+    {id: 3, title: 'Frontend development'},
+    {id: 4, title: 'Laptop environment'}
+  ]
+  public filteredTasks = [...this.tasks];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  searchTasks(e: any) {
+    const val = e.target.value;
+    if (!val) {
+      return this.filteredTasks = [...this.tasks];
+    }
+    return this.filteredTasks = this.tasks.filter(f => f.title.toLowerCase().indexOf(val) > -1);
+    
+  }
+
+}
